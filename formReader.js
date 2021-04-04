d82d7c4d9e4c76c6bc5d601bcaaa9096886eb579
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2021-04-04 14:48:45 -0400

    added alert

--- formReader.js
+++ formReader.js
@@ -3,6 +3,7 @@ for (var x = 0; x < forms.length; x++) {
   let f = forms[x];
   f.addEventListener("submit", function (e) {
     e.preventDefault();
+    alert("You submitted your form. Check your inspector console.");
     new FormData(e.target);
     let data = new FormData(e.target);
     for (var key of data.keys()) {
