20c24f33bf8013260c03aed2d6ba168ea9287e98
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2021-04-06 01:02:43 -0400

    better box grays

--- formReader.js
+++ formReader.js
@@ -1,13 +1,15 @@
-let forms = document.getElementsByTagName("form");
-for (var x = 0; x < forms.length; x++) {
-  let f = forms[x];
-  f.addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("You submitted your form. Check your inspector console.");
-    new FormData(e.target);
-    let data = new FormData(e.target);
-    for (var key of data.keys()) {
-      console.log(key, data.get(key));
-    }
-  });
-}
+window.onload = function () {
+  let forms = document.getElementsByTagName("form");
+  for (var x = 0; x < forms.length; x++) {
+    let f = forms[x];
+    f.addEventListener("submit", function (e) {
+      e.preventDefault();
+      alert("You submitted your form. Check your inspector console.");
+      new FormData(e.target);
+      let data = new FormData(e.target);
+      for (var key of data.keys()) {
+        console.log(key, data.get(key));
+      }
+    });
+  }
+};
